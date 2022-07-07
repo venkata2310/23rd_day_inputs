555537aa1523a848fd1a283239b672693ef505a2
VENKATA RAGURAM KN <107139560+venkata2310@users.noreply.github.com>
2022-07-07 20:07:24 +0530

    Add files via upload

---
+++ input_ways.py
@@ -0,0 +1,18 @@
+# q=input('Enter your name:')
+# print('My name is',q)
+# print('my name is',q,sep='-')
+# print('My name is',int(q))
+# print('My name is',float(q))
+# print('My name is'+('  ')+str(q))
+
+# # a=input('enter your mobile number:')
+# # print('is your moblie nuber',a)
+# # print('is your moblie nuber',a,sep=('_'))
+# # print('is your moblie nuber',int(a))
+# # print('is your moblie nuber',float(a))
+
+# name=input('Enter your name: ')
+# age= input('Enter your age: ')
+# per= input('Ener your percentage: ')
+
+# print(name, age, per)
